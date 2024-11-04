1da427a12b328e76ecc6f07471e3cebd5c0b361f
Seongnam-si <hongdev26@gmail.com>
2024-11-04 10:22:27 +0900

    Style(리뷰 사항 반영) : 리뷰 사항 반영하여 변수 선언문 위치 변경

---
+++ src/components/Chuck.jsx
@@ -0,0 +1,43 @@
+import * as THREE from "three";
+
+const Chuck = ({ position, color, rotation, meshRef }) => {
+  const YVERTEX = Math.sqrt(3);
+
+  const vertexArray = new Float32Array([
+    0, 0, 0,
+    2, 0, 0,
+    1, YVERTEX, 0,
+    0, 0, 2,
+    2, 0, 2,
+    1, YVERTEX, 2,
+  ]);
+
+  const shapeFace = [
+    0, 1, 2,
+    3, 4, 5,
+    0, 1, 3,
+    1, 4, 5,
+    1, 2, 5,
+    2, 3, 5,
+  ];
+
+  const customGeometry = new THREE.BufferGeometry();
+  customGeometry.setAttribute("position", new THREE.BufferAttribute(vertexArray, 3));
+  customGeometry.setIndex(shapeFace);
+
+  const shapeFaceEdgeLine = new THREE.EdgesGeometry(customGeometry);
+
+  return(
+    <>
+      <mesh geometry={customGeometry} position={position} rotation={rotation} ref={meshRef}>
+        <meshBasicMaterial color={color} side={THREE.DoubleSide}/>
+      </mesh>
+      
+      <lineSegments geometry={shapeFaceEdgeLine} position={position} rotation={rotation}>
+        <lineBasicMaterial color="black"/>
+      </lineSegments>
+    </>
+  );
+};
+
+export default Chuck;
