8703dc4d51a652c36954318931fd71246960e985
miinje <njjj00083@gmail.com>
2024-11-04 20:37:33 +0900

    style(Route): App 컴포넌트에서 Simulator Route 경로 수정
    + Main 컴포넌트에서 App 경로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/simulation" element={<Simulator />} />
+      <Route path="/simulator" element={<Simulator />} />
     </Routes>
   );
 };
