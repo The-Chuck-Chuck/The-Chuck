f1c4b2f1e766ec2c100b2c593d95a9d4cc13f059
miinje <njjj00083@gmail.com>
2024-11-04 15:00:12 +0900

    Design(Header): Header UI 디자인 완료
    - Header에서 사용할 svg 이미지 추가
    - Header UI 디자인 생성
    - index.html에서 파비콘(link 태그) 삭제
    - index.css에서 전체 배경색을 #100127로 변경

--- src/components/Simulation/Simulator.jsx
+++ src/components/Simulation/Simulator.jsx
@@ -1,5 +1,11 @@
+import Header from "../Header/Header";
+
 const Simulator = () => {
-  return <div>Hello!</div>;
+  return (
+    <div className="bg-{#242424} w-[100%] h-[100vh] text-white">
+      <Header />
+    </div>
+  );
 };
 
 export default Simulator;
