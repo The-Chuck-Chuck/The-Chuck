8e6c18aa738f523ecc9093490344b4ae65e7d610
miinje <njjj00083@gmail.com>
2024-11-04 22:22:46 +0900

    Style(Button): buttonOrSubmit -> choiceButtonOrSubmit 변수명 변경

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
-const Button = ({ buttonOrSubmit, children, handler, addClassName }) => {
+const Button = ({ choiceButtonOrSubmit, children, handler, addClassName }) => {
   return (
     <button
-      type={buttonOrSubmit === undefined ? "button" : "submit"}
+      type={choiceButtonOrSubmit === undefined ? "button" : "submit"}
       onClick={handler}
       className={`text-white border-4 font-bold hover:bg-white hover:text-black rounded-md ${addClassName}`}
     >
