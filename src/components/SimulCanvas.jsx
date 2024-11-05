063a8b1b06cd48d8d442faee6b3f5d960e5fcf8d
Frogman113 <bainstud@naver.com>
2024-11-05 20:56:37 +0900

    Style(팀 회의 변경사항): PR관련 팀 회의 후 회의 내용을 토대로 코드 정리
    1. 사용하지 않는 패키지 삭제
    2. 테스트를 위한 임시 버튼 삭제
    3. 하나의 그룹만이 목적이기 때문에 한 그룹만 유지
    4. 작업 편의성을 위한 axisHelper와 gridHelper 삭제
    5. 많은 수의 삼각기둥은 현재 필요치 않기 때문에 3개의 삼각기둥만 유지

--- src/components/SimulCanvas.jsx
+++ src/components/SimulCanvas.jsx
@@ -1,81 +1,22 @@
-import * as THREE from "three";
 import { OrbitControls } from "@react-three/drei";
-import { Canvas, useFrame } from "@react-three/fiber";
-import Chuck from "../components/Chuck";
+import { Canvas } from "@react-three/fiber";
 import { useRef, useState } from "react";
+import Chuck from "../components/Chuck";
 
 const SimulCanvas = () => {
   const [groupRotation, setGroupRotation] = useState([]);
-  const rotateVector = new THREE.Vector3(18, -3.14, -0.3).normalize();
-  const rotateVector2 = new THREE.Vector3(26, -1.14, -0.3).normalize();
 
   const groupRef1 = useRef();
-  const groupRef2 = useRef();
-  const groupRef3 = useRef();
-
-  const handleClickGroup = () => {
-    groupRef1.current.rotation.z += 1;
-    setGroupRotation();
-  };
-
-  const handleClickGroup2 = () => {
-    groupRef2.current.rotateOnWorldAxis(rotateVector, Math.PI / 2);
-    setGroupRotation();
-  };
-
-  const handleClickGroup3 = () => {
-    groupRef3.current.rotateOnWorldAxis(rotateVector2, Math.PI / 2);
-    setGroupRotation();
-  };
 
   return (
-    <>
-      <Canvas>
-        <OrbitControls />
-        <group ref={groupRef1}>
-          <Chuck position={[0, -1, -1]} color="green" />
-          <Chuck position={[2, -1, -1]} color="red" rotation={[0, 0, 1.05]} />
-          <group ref={groupRef2}>
-            <Chuck position={[2, -1, -1]} color="green" />
-            <Chuck position={[4, -1, -1]} color="red" rotation={[0, 0, 1.05]} />
-            <Chuck position={[4, -1, -1]} color="green" />
-            <Chuck position={[6, -1, -1]} color="red" rotation={[0, 0, 1.05]} />
-            <group ref={groupRef3}>
-              <Chuck position={[6, -1, -1]} color="green" />
-              <Chuck
-                position={[8, -1, -1]}
-                color="red"
-                rotation={[0, 0, 1.05]}
-              />
-              <Chuck position={[8, -1, -1]} color="green" />
-            </group>
-          </group>
-        </group>
-        <axesHelper args={[10]} />
-        <gridHelper args={[20, 20, 0xff0000, "teal"]} />
-      </Canvas>
-      <button
-        type="button"
-        onClick={handleClickGroup}
-        className="fixed top-2 flex items-center justify-center w-[100px] bg-gray-500"
-      >
-        그룹1 돌려
-      </button>
-      <button
-        type="button"
-        onClick={handleClickGroup2}
-        className="fixed top-10 flex items-center justify-center w-[100px] bg-gray-500"
-      >
-        그룹2 돌려
-      </button>
-      <button
-        type="button"
-        onClick={handleClickGroup3}
-        className="fixed top-[75px] flex items-center justify-center w-[100px] bg-gray-500"
-      >
-        그룹3 돌려
-      </button>
-    </>
+    <Canvas>
+      <OrbitControls />
+      <group ref={groupRef1}>
+        <Chuck position={[0, -1, -1]} color="green" />
+        <Chuck position={[2, -1, -1]} color="red" rotation={[0, 0, 1.05]} />
+        <Chuck position={[2, -1, -1]} color="green" />
+      </group>
+    </Canvas>
   );
 };
 
