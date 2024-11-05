d2b28f4001888b604ccbf670c95a8788a8fb1ccb
Frogman113 <bainstud@naver.com>
2024-11-05 15:25:20 +0900

    Feat(도형 그룹화): 삼각기둥 여러개를 한 그룹으로 회전 및 위치 이동 검증 및 구현

--- src/components/Chuck.jsx
+++ src/components/Chuck.jsx
@@ -1,33 +1,36 @@
+import { useFrame } from "@react-three/fiber";
 import * as THREE from "three";
 
-const Chuck = ({ position, color, rotation, meshRef }) => {
+const Chuck = ({ color, position, rotation, meshRef }) => {
   const YVERTEX = Math.sqrt(3);
-  const customGeometry = new THREE.BufferGeometry();
-  const shapeFaceEdgeLine = new THREE.EdgesGeometry(customGeometry);
   // prettier-ignore
   const vertexArray = new Float32Array([
-    0, 0, 0,
-    2, 0, 0,
-    1, YVERTEX, 0,
-    0, 0, 2,
-    2, 0, 2,
-    1, YVERTEX, 2,
-  ]);
+      0, 0, 0,
+      2, 0, 0,
+      1, YVERTEX, 0,
+      0, 0, 2,
+      2, 0, 2,
+      1, YVERTEX, 2,
+    ]);
   // prettier-ignore
   const shapeFace = [
-    0, 1, 2,
-    3, 4, 5,
-    0, 1, 3,
-    1, 4, 5,
-    1, 2, 5,
-    2, 3, 5,
-  ];
+      0, 1, 2,
+      1, 4, 2,
+      2, 5, 4,
+      5, 4, 3,
+      5, 3, 2,
+      2, 0, 3,
+      0, 1, 4,
+      0, 3, 4
+    ];
 
+  const customGeometry = new THREE.BufferGeometry();
   customGeometry.setAttribute(
     "position",
     new THREE.BufferAttribute(vertexArray, 3)
   );
   customGeometry.setIndex(shapeFace);
+  const shapeFaceEdgeLine = new THREE.EdgesGeometry(customGeometry);
 
   return (
     <>
@@ -39,7 +42,6 @@ const Chuck = ({ position, color, rotation, meshRef }) => {
       >
         <meshBasicMaterial color={color} side={THREE.DoubleSide} />
       </mesh>
-
       <lineSegments
         geometry={shapeFaceEdgeLine}
         position={position}
--- src/components/SimulCanvas.jsx
+++ src/components/SimulCanvas.jsx
@@ -1,15 +1,38 @@
 import { OrbitControls } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
+import Chuck from "../components/Chuck";
+import { useRef, useState } from "react";
 
 const SimulCanvas = () => {
+  const [xRotation, setXRotation] = useState([]);
+  const groupRef = useRef();
+
+  const handleClickGroup = () => {
+    groupRef.current.rotation.x += 1;
+  };
+
   return (
-    <Canvas>
-      <OrbitControls />
-      <mesh>
-        <cylinderGeometry args={[0.3, 0.3, 0.5, 3]} />
-        <meshBasicMaterial color="#2e7e62" />
-      </mesh>
-    </Canvas>
+    <>
+      <Canvas>
+        <OrbitControls />
+        <group rotation={[3, 4, 4]} ref={groupRef}>
+          <Chuck position={[0, 0, 0]} color="green" />
+          <Chuck position={[2, 0, 0]} color="red" rotation={[0, 0, 1.05]} />
+          <Chuck position={[2, 0, 0]} color="green" />
+          <Chuck position={[4, 0, 0]} color="red" rotation={[0, 0, 1.05]} />
+          <Chuck position={[4, 0, 0]} color="green" />
+        </group>
+        <axesHelper args={[10]} />
+        <gridHelper args={[20, 20, 0xff0000, "teal"]} />
+      </Canvas>
+      <button
+        type="button"
+        onClick={handleClickGroup}
+        className="fixed top-5 flex items-center justify-center w-[50px] bg-gray-500"
+      >
+        돌려
+      </button>
+    </>
   );
 };
 
