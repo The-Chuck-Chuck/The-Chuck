aaeeca28cc42e1f39bd64b26e786cb01b2580a78
Seongnam-si <hongdev26@gmail.com>
2024-11-05 10:47:30 +0900

    Feat(시뮬레이션 화면) : 회전 테스트를 위한 삼각기둥 관련 컴포넌트 사용 및 회전 로직 작성

--- src/components/SimulCanvas.jsx
+++ src/components/SimulCanvas.jsx
@@ -1,12 +1,35 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { useEffect, useState } from "react";
+import * as CONSTANTS from "../constants/constants.js";
 import Chuck from "./Chuck";
+import ReverseChuck from "./ReverseChuck";
 
 const SimulCanvas = () => {
+  const [rotationAngle, setRotationAngle] = useState(0);
+
+  useEffect(() => {
+    const testInterval = setInterval(() => {
+      setRotationAngle((preAngle) => preAngle + 90 * CONSTANTS.DEGREE);
+    }, 3000);
+    return () => clearInterval(testInterval);
+  }, []);
+
   return (
-    <Canvas>
+    <Canvas
+      camera={{
+        position: [2, 5, 5],
+        fov: 100,
+      }}
+    >
       <Chuck position={[0, 0, 0]} color="green" />
+      <ReverseChuck
+        position={[0, 0, 0]}
+        color="red"
+        rotationAngle={rotationAngle}
+      />
       <OrbitControls />
+      <axesHelper scale={10} />
     </Canvas>
   );
 };
