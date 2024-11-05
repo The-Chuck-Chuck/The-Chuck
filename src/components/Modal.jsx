116aa242ca75900d644818b4de3957e12913c693
miinje <njjj00083@gmail.com>
2024-11-05 16:44:54 +0900

    Design(Modal): Modal 컴포넌트 title 크기 변경
    + setIsOpenedModal 불러오는 코드 수정(구조 분해 할당 사용하지 않음)

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,7 +2,7 @@ import useStore from "../store/pageStore";
 import Button from "./Button";
 
 const Modal = ({ drection, title, addClassName, children }) => {
-  const { setIsOpenedModal } = useStore();
+  const setIsOpenedModal = useStore((state) => state.setIsOpenedModal);
   const modalDrection = {
     row: "w-[500px] h-[300px]",
     col: "w-[400px] h-[500px]",
@@ -17,7 +17,7 @@ const Modal = ({ drection, title, addClassName, children }) => {
       className={`${modalDrection[drection]} ${addClassName} fixed text-white flex flex-col bg-gray-700 rounded-xl border-gray-400 border-4 p-4 z-10`}
     >
       <div className="flex justify-between items-center mb-4">
-        <h2 className="text-lg font-bold">{title}</h2>
+        <h2 className="text-xl font-semibold">{title}</h2>
         <Button handler={handleClickClose} addClassName="border-2 w-8">
           X
         </Button>
