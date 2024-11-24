5589e491e3fd8772d88304b9708800544a691fdb
miinje <njjj00083@gmail.com>
2024-11-25 01:32:37 +0900

    Design(ShareUrlModal): 안내 문구 추가

--- src/pages/Modal/ShareUrlModal.jsx
+++ src/pages/Modal/ShareUrlModal.jsx
@@ -29,21 +29,26 @@ const ShareUrlModal = ({ isOpenedShare, setIsOpenedShare }) => {
       title="Share Your Chuck-chuck!"
       setIsOpened={setIsOpenedShare}
     >
-      <div className="flex gap-3 justify-center items-center w-[100%] h-[100%]">
-        <input
-          type="text"
-          ref={dataLinkRef}
-          value={`${window.location.origin}${window.location.pathname}/${encodedPositionsData}`}
-          className="text-white w-80 h-11 p-2 bg-gray-500 rounded-md justify-center"
-          disabled={true}
-        />
-        <Button
-          clickHandler={handleClickCopy}
-          className={`${isCopied && "bg-green-400 border-white text-white"} h-11 pl-2 pr-2`}
-          disabled={isCopied && true}
-        >
-          {isCopied ? "copied! ✅" : "Copy Link!"}
-        </Button>
+      <div className="w-[100%] h-[100%] flex flex-col justify-center items-center gap-6">
+        <p className="text-lg font-semibold">
+          아래 링크를 복사해 공유해 보세요!
+        </p>
+        <div className="flex gap-3 justify-center items-center">
+          <input
+            type="text"
+            ref={dataLinkRef}
+            value={`${window.location.origin}${window.location.pathname}/${encodedPositionsData}`}
+            className="text-white w-80 h-11 p-2 bg-gray-500 rounded-md justify-center"
+            disabled={true}
+          />
+          <Button
+            clickHandler={handleClickCopy}
+            className={`${isCopied && "bg-green-400 border-white text-white"} h-11 pl-2 pr-2`}
+            disabled={isCopied && true}
+          >
+            {isCopied ? "copied! ✅" : "Copy Link!"}
+          </Button>
+        </div>
       </div>
     </Modal>
   );
