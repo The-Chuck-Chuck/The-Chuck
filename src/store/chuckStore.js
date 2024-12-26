77e21ea31433901da41e62675f3f4ba23b1fd094
miinje <njjj00083@gmail.com>
2024-12-27 05:33:12 +0900

    Style(chuckStore): isSharedLinks 삭제
    - isSharedLinks 삭제 및 사용하던 곳에서 제거
    - 더이상 공유 기능을 사용하지 않아 필요하지 않음

--- src/store/chuckStore.js
+++ src/store/chuckStore.js
@@ -4,10 +4,8 @@ import { persist } from "zustand/middleware";
 const useChuckStore = create(
   persist((set) => ({
     chuckPositionsList: [],
-    isSharedLinks: false,
     isClickSkip: false,
     setChuckPositionsList: (state) => set({ chuckPositionsList: state }),
-    setIsSharedLinks: (state) => set({ isSharedLinks: state }),
     setIsClickSkip: (state) => set({ isClickSkip: state }),
   }))
 );
