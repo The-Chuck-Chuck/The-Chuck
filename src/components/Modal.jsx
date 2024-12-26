65e356ecf27578657b0e066a3c5197b5e6620899
miinje <njjj00083@gmail.com>
2024-12-27 02:58:56 +0900

    Design: 튜토리얼 시작 안내 모달 문구 작성 완료
    - 안내 문구와 맞는 이미지 파일 추가
    - Modal 컴포넌트에서 튜토리얼일 시 title 제거

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -16,7 +16,9 @@ const Modal = ({ drection, title, className, setIsOpened, children }) => {
         className={`${modalDrection[drection]} ${drection === "horizontal" && "top-1/3 left-[33%]"} ${drection === "vertical" && "top-5.5 left-[35%]"} ${className} fixed text-white flex flex-col bg-gray-700 rounded-xl border-gray-400 border-4 p-4 z-20`}
       >
         <div className="flex justify-between items-center mb-4">
-          <h2 className="text-2xl font-semibold">{title}</h2>
+          {!setIsOpened && drection === "vertical" ? null : (
+            <h2 className="text-2xl font-semibold">{title}</h2>
+          )}
           {setIsOpened ? (
             <Button clickHandler={handleClickClose} className="border-2 w-8">
               X
--- src/pages/Modal/StartTutorialModal.jsx
+++ src/pages/Modal/StartTutorialModal.jsx
@@ -1,9 +1,12 @@
 import * as THREE from "three";
-import tempChuckImage from "../../asset/chuckChuckModel.png";
+import highlightChuckGif from "../../asset/highlight-chuck.gif";
+import clickButtonDirection from "../../asset/clickButtonDirection.png";
+import chuckChuckModel from "../../asset/chuckChuckModel.png";
 import Button from "../../components/Button";
 import Modal from "../../components/Modal";
 import useChuckStore from "../../store/chuckStore";
 import usePageStore from "../../store/pageStore";
+import { useState } from "react";
 
 const StartTutorialModal = () => {
   const setIsOpenedTutorialModal = usePageStore(
@@ -12,6 +15,14 @@ const StartTutorialModal = () => {
   const setChuckPositionsList = useChuckStore(
     (state) => state.setChuckPositionsList
   );
+  const [pageCount, setPageCount] = useState(0);
+
+  const handleClickNextPage = () => {
+    setPageCount(pageCount + 1);
+  };
+  const handleClicPrevPage = () => {
+    setPageCount(pageCount - 1);
+  };
 
   const handleClickStartTutorial = () => {
     const initialValue = 24;
@@ -31,21 +42,81 @@ const StartTutorialModal = () => {
 
     setChuckPositionsList(initialStateArray);
     setIsOpenedTutorialModal(false);
+    setPageCount(0);
   };
 
   return (
     <Modal drection="vertical" title="Start Tutorial">
-      <div className="flex w-[90%] h-[100vh] flex-col justify-center items-center ml-2 gap-3">
-        <h2 className="text-center text-2xl font-bold">강아지 만들기</h2>
-        <img className="m-7 w-24" src={tempChuckImage} alt="chuckimage" />
-        <div className="w-64 text-center text-md">
-          하이라이트 된 도형을 선택하고, 버튼을 클릭하세요! 순서를 따라가면
-          강아지를 만들 수 있어요!
-        </div>
+      <div className="flex h-[100%] flex-col items-center gap-3">
+        <h2 className="text-center text-3xl font-semibold mb-3">
+          Start Tuorial!
+        </h2>
+        {pageCount === 0 && (
+          <>
+            <img
+              className="m-2 w-56"
+              src={highlightChuckGif}
+              alt="highlight chuck gif"
+            />
+            <p className="w-64 text-center text-lg">
+              <span className="font-bold">반짝이는 도형</span>을 클릭하세요!{" "}
+              <br />
+              우린 이 도형까지 회전시킬 거예요.
+            </p>
+          </>
+        )}
+        {pageCount === 1 && (
+          <>
+            <img
+              className="mt-7 mb-2 w-64"
+              src={clickButtonDirection}
+              alt="click Button Direction"
+            />
+            <p className="w-72 text-center text-lg">
+              그 다음 <span className="font-bold">켜진 방향 버튼</span>을 클릭해
+              주세요.
+            </p>
+            <p className="w-72 text-center text-lg">
+              선택한 방향으로 척척이가 회전합니다!
+            </p>
+            <p className="w-72 text-center text-lg">
+              이제 다음 도형을 클릭할 수 있습니다!
+            </p>
+          </>
+        )}
+        {pageCount === 2 && (
+          <div className="flex flex-col h-[100%] justify-center items-center gap-5">
+            <img
+              className="m-2 w-48"
+              src={chuckChuckModel}
+              alt="chuckChuck Model"
+            />
+            <p className="w-80 text-center text-md">
+              순서대로 따라가면 강아지를 만들 수 있습니다.
+            </p>
+            <p className="w-64 text-center text-lg font-semibold">
+              이제 튜토리얼을 시작해 보세요!
+            </p>
+          </div>
+        )}
       </div>
-      <Button clickHandler={handleClickStartTutorial} className="p-2 text-md">
-        Start Tutorial!
-      </Button>
+
+      <div className="flex gap-4 w-[100%]">
+        <Button
+          clickHandler={handleClicPrevPage}
+          className="w-[50%] p-2 text-md mb-3"
+        >
+          Prev Page
+        </Button>
+        <Button
+          clickHandler={
+            pageCount !== 2 ? handleClickNextPage : handleClickStartTutorial
+          }
+          className="w-[50%] p-2 text-md mb-3"
+        >
+          {pageCount !== 2 ? "Next Page" : "Start Tutorial!"}
+        </Button>
+      </div>
     </Modal>
   );
 };
