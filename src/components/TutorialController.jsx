108e6d3c6799c2732bd16788774502754b8768a4
Seongnam-si <hongdev26@gmail.com>
2024-12-31 14:36:24 +0900

    Design: 컨트롤박스 부분 UI 변경 1. 영어 => 한글로 변경 2. 기존 회전 left, right 버튼 기호로 변경 3. 기호 변경에 따른 이미지 추가 및 수정

--- src/components/TutorialController.jsx
+++ src/components/TutorialController.jsx
@@ -1,5 +1,7 @@
 import * as CONSTANTS from "../constants/constants";
 import Button from "./Button";
+import left from "../asset/left-rotate.svg";
+import right from "../asset/right-rotate.svg";
 
 const TutorialController = ({
   essentialClickButton,
@@ -25,26 +27,27 @@ const TutorialController = ({
   return (
     <div className="flex flex-col gap-4 fixed bottom-4 right-4 w-[250px] rounded-md bg-slate-600 p-5 justify-center items-center">
       <div className="w-[90%] flex gap-2 items-center">
-        <p className="pl-[2%] pr-[15%] text-center font-bold text-lg">Turn!</p>
-        <Button
-          className="h-10 p-1"
-          clickHandler={handleClickLeft}
-          disabled={
-            (!isCompletedIndex || essentialClickButton === "right") &&
-            "disabled"
-          }
-        >
-          Left
-        </Button>
-        <Button
-          className="h-10 p-1"
-          clickHandler={handleClickRight}
-          disabled={
-            (!isCompletedIndex || essentialClickButton === "left") && "disabled"
-          }
-        >
-          Right
-        </Button>
+        <p className="pl-[2%] pr-[15%] text-center font-bold text-lg">회전</p>
+        <button onClick={handleClickLeft} className="h-10 p-1 pr-[10%]">
+          <img
+            src={left}
+            className={`h-full ${
+              !isCompletedIndex || essentialClickButton === "right"
+                ? "opacity-30"
+                : "opacity-100 hover:opacity-75"
+            }`}
+          />
+        </button>
+        <button onClick={handleClickRight} className="h-10 p-1">
+          <img
+            src={right}
+            className={`h-full ${
+              !isCompletedIndex || essentialClickButton === "left"
+                ? "opacity-30"
+                : "opacity-100 hover:opacity-75"
+            }`}
+          />
+        </button>
       </div>
     </div>
   );
